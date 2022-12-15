938ea28194e5abc276f7d9108637a9c0ef5a357d
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2022-12-14 20:30:46 -0500

    simple test UDP server

--- OpenGLMadness/Server/main.cpp
+++ OpenGLMadness/Server/main.cpp
@@ -8,15 +8,30 @@
 #include <iphlpapi.h>
 #include <stdio.h>
 
+#include <vector>
+
+#include "../UDPLibrary/Buffer.h"
+
 #pragma comment(lib, "Ws2_32.lib")
 
 #define DEFAULT_PORT "27015"
+
+
+#define BUFLEN 512	//Max length of buffer
+#define PORT 8888	//The port on which to listen for incoming data
+
+char buf[BUFLEN];
+int slen, recv_len;
 
 int main()
 {
 	WSADATA wsaData;
 
+	std::vector<sockaddr_in> clients;
 	int iResult;
+
+
+	slen = sizeof(clients);
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -47,16 +62,76 @@ int main()
 	SOCKET ListenSocket = INVALID_SOCKET;
 
 
-	//make a socket with what we could get based on the hints we had
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-	//safety check
-	if (ListenSocket == INVALID_SOCKET)
+	//Create a socket
+	if ((ListenSocket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
-		printf("Error at socket(): %ld\n", WSAGetLastError());
+		printf("Could not create socket : %d", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+	}
+	printf("Socket created.\n");
+
+	sockaddr_in server;
+
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(PORT);
+
+	//Bind
+	if (bind(ListenSocket, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d", WSAGetLastError());
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
+	}
+	puts("Bind done");
+
+
+	// Set our socket to be nonblocking
+	ULONG NonBlock = 1;
+	int r = ioctlsocket(ListenSocket, FIONBIO, &NonBlock);
+	if (r == SOCKET_ERROR)
+	{
+		printf("Non-blocking failed with error code : %d", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	//everything should be set up now
+
+	while(1)
+	{
+		memset(buf, '\0', BUFLEN);
+
+		sockaddr_in client;
+
+		int result = recv_len = recvfrom(ListenSocket, buf, BUFLEN, 0, (struct sockaddr*)&client, &slen);
+		if (result == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+
+			// For a TCP connection you would close this socket, and remove it from 
+			// your list of connections. For UDP we will clear our buffer, and just
+			// ignore this.
+			memset(buf, '\0', 512);
+			//exit(EXIT_FAILURE);
+			return 1;
+		}
+		else
+		{
+			Buffer incoming(BUFLEN);
+			incoming.LoadBuffer(buf, BUFLEN);
+
+			std::string line = incoming.PayloadToString();
+			int stop = 0;
+
+		}
 	}
 
 	return 0;
---
+++ OpenGLMadness/UDPLibrary/Buffer.cpp
@@ -0,0 +1,197 @@
+//Buffer.cpp
+//Gian Tullo, 0886424 / Lucas Magalhaes / Philip Tomaszewski
+//23/10/21
+//Provides the functionality of a TCP Packet buffer
+//  See header for method Manifest
+
+#include "Buffer.h"
+
+Buffer::Buffer(size_t size)
+{
+	buffer = std::vector<uint8_t>(size);
+	indexRead = 0;
+	indexWrite = 0;
+}
+
+void Buffer::writeUInt32BE(std::size_t index, int32_t value)
+{
+	buffer.insert(buffer.begin() + index, value >> 24);
+	buffer.insert(buffer.begin() + index + 1, value >> 16);
+	buffer.insert(buffer.begin() + index + 2, value >> 8);
+	buffer.insert(buffer.begin() + index + 3, value);
+
+	indexWrite += 4;
+}
+
+void Buffer::writeUInt32BE(int32_t value)
+{
+	if (indexWrite >= buffer.size())
+	{
+		buffer.resize(indexWrite + 4);
+	}
+
+	buffer[indexWrite] = value >> 24;
+	buffer[indexWrite + 1] = value >> 16;
+	buffer[indexWrite + 2] = value >> 8;
+	buffer[indexWrite + 3] = value;
+
+	indexWrite += 4;
+}
+
+uint32_t Buffer::readUInt32BE(std::size_t index)
+{
+	uint32_t value = buffer[index] << 24;
+	value |= buffer[index + 1] << 16;
+	value |= buffer[index + 2] << 8;
+	value |= buffer[index + 3];
+
+	return value;
+}
+
+uint32_t Buffer::readUInt32BE()
+{
+	uint32_t value = buffer[indexRead] << 24;
+	value |= buffer[indexRead + 1] << 16;
+	value |= buffer[indexRead + 2] << 8;
+	value |= buffer[indexRead + 3];
+
+	indexRead += 4;
+	return value;
+}
+
+void Buffer::writeUInt16BE(std::size_t index, int16_t value)
+{
+
+	buffer.insert(buffer.begin() + index, value >> 8);
+	buffer.insert(buffer.begin() + index + 1, value);
+
+	indexWrite += 2;
+}
+
+void Buffer::writeUInt16BE(int16_t value)
+{
+	if (indexWrite >= buffer.size())
+	{
+		buffer.resize(indexWrite + 2);
+	}
+	buffer[indexWrite] = value >> 8;
+	buffer[indexWrite + 1] = value;
+
+	indexWrite += 2;
+}
+
+uint16_t Buffer::readUInt16BE(std::size_t index)
+{
+	uint16_t value = buffer[index] << 8;
+	value |= buffer[index + 1];
+
+	return value;
+}
+
+uint16_t Buffer::readUInt16BE()
+{
+	uint16_t value = buffer[indexRead] << 8;
+	value |= buffer[indexRead + 1];
+
+	indexRead += 2;
+	return value;
+}
+
+void Buffer::writeUInt8BE(std::size_t index, std::string value)
+{
+	int charOrder = 0;
+	for (char c : value)
+	{
+		//buffer[index + charOrder] = c;
+		buffer.insert(buffer.begin() + index + charOrder, c);
+		charOrder++;
+	}
+
+	indexWrite += charOrder;
+}
+
+void Buffer::writeUInt8BE(std::string value)
+{
+	if (indexWrite >= buffer.size())
+	{
+		buffer.resize(indexWrite + value.length());
+	}
+
+	int charOrder = 0;
+	for (char c : value)
+	{
+		buffer[indexWrite + charOrder] = c;
+		charOrder++;
+	}
+
+	indexWrite += charOrder;
+}
+
+std::string Buffer::readUInt8BE(std::size_t index, int stringSize)
+{
+	std::string output;
+	for (int i = 0; i < stringSize; i++)
+	{
+		output.push_back(buffer[index + i]);
+	}
+
+	return output;
+}
+
+std::string Buffer::readUInt8BE(int stringSize)
+{
+	std::string output = "";
+	for (int i = 0; i < stringSize; i++)
+	{
+		output.push_back(buffer[indexRead]);
+		indexRead++;
+	}
+
+	return output;
+}
+
+std::vector<uint8_t> Buffer::GetBuffer()
+{
+	return buffer;
+}
+
+int Buffer::GetWriteIndex()
+{
+	return indexWrite;
+}
+
+char* Buffer::PayloadToString()
+{
+	int totalLength = readUInt32BE();
+	char* outbound = new char[totalLength];
+	for (int i = 0; i < totalLength; i++)
+	{
+		outbound[i] = buffer[i];
+	}
+	return outbound;
+}
+
+void Buffer::LoadBuffer(char* recvd, size_t leght)
+{
+	ClearBuffer();
+	//for (char c : recvd)
+	//{
+	//	buffer.push_back(c);
+	//}
+	for (int i = 0; i < leght; i++)
+	{
+		buffer.push_back(recvd[i]);
+	}
+}
+
+void Buffer::ClearBuffer()
+{
+	buffer.clear();
+	indexRead = 0;
+	indexWrite = 0;
+}
+
+size_t Buffer::GetBufferSize()
+{
+	return indexWrite;
+}
